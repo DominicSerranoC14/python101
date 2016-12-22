4dd8c371558405e6a08b54df1bf2243b3fd56b6a
Dominic Serrano <dmsrojo@gmail.com>
2016-12-21 22:09:05 -0600

    Used the slice, remove and del methods with a list

--- foundations/exercises/planetList.py
+++ foundations/exercises/planetList.py
@@ -13,11 +13,16 @@ planet_list.insert(2, 'Earth');
 
 # Use append() again to add Pluto to the end of the list.
 planet_list.append('Pluto');
-print(planet_list);
 
 # Now that all the planets are in the list, slice the list in order to get the rocky planets into a new list called rocky_planets.
-# Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the del operation to remove it from the end of planet_list.
-# Iterating over planets
+rocky_planets = planet_list[0:4];
+print(rocky_planets);
 
+# Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the del operation to remove it from the end of planet_list.
+# planet_list.remove('Pluto');
+del planet_list[8];
+print(planet_list);
+
+# Iterating over planets
 # Create another list containing tuples. Each tuple will hold the name of a spacecraft that we have launched, and the names of the planet(s) that it has visited, or landed on. (e.g. ('Cassini', 'Jupiter')).
 # Iterate over your list of planets, and inside that loop, iterate over the list of tuples. Print, for each planet, which sattelites have visited.
