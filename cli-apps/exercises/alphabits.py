82bcba405dbab8c3617beb46b0288025e0f3fabf
Dominic Serrano <dmsrojo@gmail.com>
2016-12-29 12:06:19 -0600

    Completed alphabits exercise

--- cli-apps/exercises/alphabits.py
+++ cli-apps/exercises/alphabits.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
 
-from sys import argv;
-
-# Upon completing the task (entering in all 26 letters), the user should receive some gratifying message like "Congratulations"
-
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+alphabet = ['A', 'B', 'C', 'D', 'e', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 # Use one Class in addition to the program file.
 # Your class should include the __init__ constructor function, an add_char method, and a list_length method.
 class Alphabits:
     """
+    Class which creates a new alphabet game.
     """
 
     def __init__(self):
+        '''
+        Inits and starts the game.
+        '''
         self.input_list = [];
         self.init_game();
+        self.game_play();
 
     # Keep your user's successfully input letters as a list of characters.
     def add_char(self, letter):
+        '''
+        If the user input is correct, append to the input_list set.
+        '''
         self.input_list.append(letter);
 
     # Use the string interpolation to craft your messages to users.
     def list_length_str(self):
+        '''
+        Return a string of the current user letter postition.
+        e.g: What is the 4th letter?
+        '''
         length = len(self.input_list) + 1;
         if length == 1:
             return '1st';
@@ -34,6 +42,7 @@ class Alphabits:
             return str(length) + 'th';
 
     def list_length(self):
+        ''' Returns an int of the input_list '''
         return len(self.input_list);
 
     # Create a return_list method on your class that will return the current list of characters that the user has entered in correctly.
@@ -56,7 +65,15 @@ class Alphabits:
         print('Alphabits is about to begin!');
         print('Please enter each letter of the alphabit one at a time.\n');
 
-    def start_game(self):
+    def determine_gameover(self):
+        # Upon completing the task (entering in all 26 letters), the user should receive some gratifying message like "Congratulations"
+        if len(alphabet) == self.list_length():
+            print('\nCongratulations!');
+            print('You have completed the game, you are an Alphabet Master.');
+        else:
+            self.game_play();
+
+    def game_play(self):
         """
         All game logic executed here.
         """
@@ -68,19 +85,21 @@ class Alphabits:
         # Create a non-letter "Easter egg" character that will display the current list of successfully input letters (but will not add itself to the list!) which calls the the return_list method.
         if user_choice == '?':
             self.return_list();
+            self.game_play();
         # If user's anser mathches current letter
         elif user_choice.upper() == alphabet[self.list_length()]:
             print('Correct!');
             # After each successful input, display the number of letters already entered.
             print('Correct answers: ' + str(self.list_length() + 1));
             self.add_char(user_choice);
+            self.determine_gameover();
+
         else:
             # After each unsuccessful input, display a helpful message explaining why the input was unsuccessful (e.g. 'not a letter', 'duplicate letter', etc.)
             self.handle_incorrect(user_choice);
 
-
-        self.start_game();
+    ##########################################
 
 
 # Begin game
-Alphabits().start_game();
+Alphabits();
