7dda16575720eaa66de37fe9db8c4f10e9d8f416
Dominic Serrano <dmsrojo@gmail.com>
2016-12-29 15:56:32 -0600

    Added basic testing suite

---
+++ foundations/resources/tests/test_info.py
@@ -0,0 +1,6 @@
+# Use this command to run all tests in the directory tests
+# Each file must be suffixed with _test.py
+# python -m unittest discover tests "*_test.py"
+
+# The following line is the same:
+# python -m unittest discover -s tests -p "*_test.py"
