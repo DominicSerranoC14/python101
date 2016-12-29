b4797b2c69549e5b1f809e90c8abffd8c3a48be8
Dominic Serrano <dmsrojo@gmail.com>
2016-12-29 11:46:31 -0600

    Gameplay complete. Adding winning status once player has entered all letters

--- cli-apps/exercises/alphabits.py
+++ cli-apps/exercises/alphabits.py
@@ -2,8 +2,6 @@
 
 from sys import argv;
 
-# After each successful input, display the number of letters already entered.
-# After each unsuccessful input, display a helpful message explaining why the input was unsuccessful (e.g. 'not a letter', 'duplicate letter', etc.)
 # Upon completing the task (entering in all 26 letters), the user should receive some gratifying message like "Congratulations"
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -38,6 +36,13 @@ class Alphabits:
     def list_length(self):
         return len(self.input_list);
 
+    # Create a return_list method on your class that will return the current list of characters that the user has entered in correctly.
+    def return_list(self):
+        collection = '';
+        for letter in self.input_list:
+            collection += letter.upper() + ' ';
+        print(collection);
+
     def handle_incorrect(self, letter):
         if letter in self.input_list:
             print('Incorrect. That letter has already been entered.');
@@ -45,31 +50,37 @@ class Alphabits:
             print('Incorrect answer. Please try again.');
 
     def init_game(self):
+        '''
+        Alerts user that game is about to start.
+        '''
         print('Alphabits is about to begin!');
         print('Please enter each letter of the alphabit one at a time.\n');
 
     def start_game(self):
         """
+        All game logic executed here.
         """
 
+        # Alert the user to which letter they are on
         print('What is the ' + self.list_length_str() + ' letter of the alphabet?');
         user_choice = input("> ");
 
-        if user_choice.upper() == alphabet[self.list_length()]:
+        # Create a non-letter "Easter egg" character that will display the current list of successfully input letters (but will not add itself to the list!) which calls the the return_list method.
+        if user_choice == '?':
+            self.return_list();
+        # If user's anser mathches current letter
+        elif user_choice.upper() == alphabet[self.list_length()]:
             print('Correct!');
+            # After each successful input, display the number of letters already entered.
             print('Correct answers: ' + str(self.list_length() + 1));
             self.add_char(user_choice);
         else:
+            # After each unsuccessful input, display a helpful message explaining why the input was unsuccessful (e.g. 'not a letter', 'duplicate letter', etc.)
             self.handle_incorrect(user_choice);
 
 
         self.start_game();
 
+
+# Begin game
 Alphabits().start_game();
-
-
-
-
-
-# Create a return_list method on your class that will return the current list of characters that the user has entered in correctly.
-# Create a non-letter "Easter egg" character that will display the current list of successfully input letters (but will not add itself to the list!) which calls the the return_list method.
